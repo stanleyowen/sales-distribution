9e11e29463cd9f24560b1eae386ba735b04c7d37
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-02-04 10:48:03 +0700

    Remove Navbar Component

--- src/components/app.component.tsx
+++ src/components/app.component.tsx
@@ -70,29 +70,6 @@ const App = ({ auth, config }: any) => {
     return (
         <div>
             <div>
-                <Box sx={{ flexGrow: 1 }}>
-                    <AppBar position="static">
-                        <Toolbar>
-                            <IconButton
-                                size="large"
-                                edge="start"
-                                color="inherit"
-                                aria-label="menu"
-                                sx={{ mr: 2 }}
-                            >
-                                <MenuIcon />
-                            </IconButton>
-                            <Typography
-                                variant="h6"
-                                component="div"
-                                sx={{ flexGrow: 1 }}
-                            >
-                                News
-                            </Typography>
-                            <Button color="inherit">Login</Button>
-                        </Toolbar>
-                    </AppBar>
-                </Box>
                 <div className="mt-10 p-10">
                     <Grid container spacing={2}>
                         <Grid item xs={8}>
