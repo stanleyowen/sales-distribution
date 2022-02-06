67652116cff4da7f94c76efae265d975017f0682
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-02-06 09:16:17 +0700

    Added Dynamic `filePath` for Read File Operation

--- src/lib/file-operation.lib.js
+++ src/lib/file-operation.lib.js
@@ -1,18 +1,12 @@
 import React from 'react';
 const fs = window.require('fs');
 
-export function readFile(callback) {
+export function readFile(filePath, callback) {
     let values = '';
 
-    fs.readFile(
-        process.env.NODE_ENV === 'development'
-            ? `./public/db/customer-data.json`
-            : `../build/db/customer-data.json`,
-        'utf-8',
-        (err, data) => {
-            if (err) throw err;
-            values = data.toString();
-            return callback(values);
-        }
-    );
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) throw err;
+        values = data.toString();
+        return callback(values);
+    });
 }
