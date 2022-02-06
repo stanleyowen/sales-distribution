4ec0ccbb559aaa6fb2290ea7ea160dfc59eb37e4
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-02-06 19:15:05 +0700

    Fixed Issue of `null` is Returned from `readFile()` Function

--- src/lib/file-operation.lib.js
+++ src/lib/file-operation.lib.js
@@ -2,10 +2,8 @@ import React from 'react';
 const fs = window.require('fs');
 
 export function readFile(filePath, callback) {
-    let values = '';
-
     fs.readFile(filePath, 'utf-8', (err, data) => {
         if (err) throw err;
-        return callback(values.toString());
+        return callback(data.toString());
     });
 }
