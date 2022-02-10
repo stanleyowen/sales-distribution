7ce8fbc5299f6acc734d591252ba11b3fbed3432
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-02-10 16:31:41 +0700

    Use `HashRouter` instead of `BrowserRouter`

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import AppLayout from './components/app.component';
 import Navbar from './components/navbar.component';
@@ -9,23 +9,15 @@ process.env.NODE_ENV === 'production'
     ? require('./App.min.css')
     : require('./App.css');
 
-interface EnvConfiguration {
-    apiKey: string | undefined;
-    authDomain: string | undefined;
-    projectId: string | undefined;
-    appId: string | undefined;
-    measurementId: string | undefined;
-}
-
 // eslint-disable-next-line
 export default function App() {
     return (
-        <Router>
+        <HashRouter>
             <Navbar />
             <Routes>
                 <Route path="/" element={<AppLayout />} />
                 <Route path="/customer" element={<Customer />} />
             </Routes>
-        </Router>
+        </HashRouter>
     );
 }
