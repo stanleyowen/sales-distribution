accdf0198b74c7bf9701d03c0973fd5b29ce06c2
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-02-10 17:17:08 +0700

    Added Upload Icon

--- src/lib/icons.component.tsx
+++ src/lib/icons.component.tsx
@@ -12,3 +12,16 @@ export function MenuIcon(
         </svg>
     );
 }
+
+export function UploadIcon(
+    props: React.SVGProps<SVGSVGElement> | null | undefined
+) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+            <path
+                d="M9 16v-6H5l7-7l7 7h-4v6H9m-4 4v-2h14v2H5z"
+                fill="currentColor"
+            ></path>
+        </svg>
+    );
+}
