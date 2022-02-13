ef8ef7cae08f14fec299e959d0beacb2217b283d
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-02-13 18:49:24 +0700

    Added File Icons

--- src/lib/icons.component.tsx
+++ src/lib/icons.component.tsx
@@ -25,3 +25,16 @@ export function UploadIcon(
         </svg>
     );
 }
+
+export function FileIcon(
+    props: React.SVGProps<SVGSVGElement> | null | undefined
+) {
+    return (
+        <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+            <path
+                d="M13 9V3.5L18.5 9M6 2c-1.11 0-2 .89-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6H6z"
+                fill="currentColor"
+            ></path>
+        </svg>
+    );
+}
