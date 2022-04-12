dcbf20997f423184a6e7b024ef8760ae8ca54242
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-04-12 17:57:46 +0700

    Breaking Changes: Replacing `render` with `createRoot`

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+ReactDOMClient.createRoot(document.getElementById('root')!).render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
